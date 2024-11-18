1562cb30f31bc1d4568821178f525935364c94ef
PaoloBellezaTelloo <165170439+PaoloBellezaTelloo@users.noreply.github.com>
2024-11-17 22:59:58 -0500

    Update IBaseRepository.cs

--- Domain/Shared/IBaseRepository.cs
+++ Domain/Shared/IBaseRepository.cs
@@ -2,10 +2,11 @@ namespace Login_back. Domain. Shared
 {
     public interface IBaseRepository<T> where T : class
     {
-        Task<T> GetByIdAsync(int id);
+        Task<T> GetByIdAsync(string id);
         Task<IEnumerable<T>> GetAllAsync();
         Task AddAsync(T entity);
         void Update(T entity);
         void Delete(T entity);
+        Task SaveChangesAsync();
     }
 }
